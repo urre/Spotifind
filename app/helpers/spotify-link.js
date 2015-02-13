86968c2bff9dad57af8f3c6d0b5413e56565a394
Urban Sanden <hej@urre.me>
2015-02-13 11:52:01 +0100

    Scoped url different

--- app/helpers/spotify-link.js
+++ app/helpers/spotify-link.js
@@ -4,9 +4,10 @@ import Ember from "ember";
 export default Ember.Handlebars.makeBoundHelper(function(uri, title) {
 
     var parts = uri.split(':');
+    var url;
 
     if (parts.length === 3 && parts[0] === "spotify") {
-        var url = "http://open.spotify.com/" + parts[1] + "/" + parts[2];
+        url = "http://open.spotify.com/" + parts[1] + "/" + parts[2];
     }
 
     return new Ember.Handlebars.SafeString('<a href="' + url + '">' + title + '</a>');
