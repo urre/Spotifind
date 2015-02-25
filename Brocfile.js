d83246e812ad87f8a173dce0c103028439589bbe
Urban Sanden <hej@urre.me>
2015-02-25 08:10:59 +0100

    Dont minify css right now, for testing

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp({
     minifyCSS: {
-       enabled: true,
+       enabled: false,
        options: {}
      },
     autoprefixer: {
